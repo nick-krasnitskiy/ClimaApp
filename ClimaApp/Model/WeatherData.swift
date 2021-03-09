417501f3db12cfa5a03fb43ecfaeae44c1a78fcf
NickKrasnitskiy <nick.krasnitskiy@gmail.com>
2021-03-09 11:10:42 +0300

    Add the JSIN Decoding

---
+++ ClimaApp/Model/WeatherData.swift
@@ -0,0 +1,23 @@
+//
+//  WeatherData.swift
+//  ClimaApp
+//
+//  Created by Nick Krasnitskiy on 09.03.2021.
+//
+
+import Foundation
+
+struct WeatherData: Decodable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+   
+}
+
+struct Main: Decodable {
+    let temp: Double
+}
+
+struct Weather: Decodable {
+    let description: String
+}
--- ClimaApp/Model/WeatherManager.swift
+++ ClimaApp/Model/WeatherManager.swift
@@ -31,17 +31,24 @@ struct WeatherManager {
                 }
                 
                 if let safeData = data {
-                    let dataString = String(data: safeData, encoding: .utf8)
-                    print(dataString)
+                    self.parseJSON(weatherData: safeData)
                 }
             }
             
             // 4. Start the task
             task.resume()
         }
-        
-        
     }
     
-    
+    func parseJSON(weatherData: Data) {
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
+            print(decodedData.name)
+            print(decodedData.main.temp)
+            print(decodedData.weather[0].description)
+        } catch {
+            print(error)
+        }
+    }
 }
