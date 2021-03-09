82813f39cfa93eafbd662403f1746fae9d76be15
NickKrasnitskiy <nick.krasnitskiy@gmail.com>
2021-03-09 09:45:42 +0300

    Add the URLSession functionality,

--- ClimaApp/Model/WeatherManager.swift
+++ ClimaApp/Model/WeatherManager.swift
@@ -8,10 +8,40 @@
 import Foundation
 
 struct WeatherManager {
-    let weatherURL = "http://api.openweathermap.org/data/2.5/weather?appid=ee416ad257d6ba655357be6e92061a28&units=metric"
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=ee416ad257d6ba655357be6e92061a28&units=metric"
     
     func fetchWeather(cityName: String) {
         let urlString = "\(weatherURL)&q=\(cityName)"
-        print(urlString)
+        performRequest(urlString: urlString)
     }
+    
+    func performRequest(urlString: String) {
+        
+        // 1. Create a URL
+        if let url = URL(string: urlString) {
+            
+            // 2. Create a URLSession
+            let session = URLSession(configuration: .default)
+            
+            // 3. Give the session a task
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                
+                if let safeData = data {
+                    let dataString = String(data: safeData, encoding: .utf8)
+                    print(dataString)
+                }
+            }
+            
+            // 4. Start the task
+            task.resume()
+        }
+        
+        
+    }
+    
+    
 }
