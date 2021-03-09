50d2063ff524bade72f53594183f8f8cb1eaa181
NickKrasnitskiy <nick.krasnitskiy@gmail.com>
2021-03-09 07:50:26 +0300

    The initial commit

---
+++ ClimaApp/Controller/WeatherViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  ClimaApp
+//
+//  Created by Nick Krasnitskiy on 01.03.2021.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLAbel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        searchTextField.delegate = self
+    }
+    
+    @IBAction func searchPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.endEditing(true)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        } else {
+            textField.placeholder = "Type something"
+            return false
+        }
+    }
+    
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        // Use searchTextField.text to get the weather for that city.
+        searchTextField.text = ""
+    }
+}
+
