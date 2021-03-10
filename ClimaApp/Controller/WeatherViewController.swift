42a4cc407814326ae4e165d6ec1da6b6d8c7ff3c
NickKrasnitskiy <nick.krasnitskiy@gmail.com>
2021-03-10 09:29:14 +0300

    The UI updating was added.

--- ClimaApp/Controller/WeatherViewController.swift
+++ ClimaApp/Controller/WeatherViewController.swift
@@ -11,7 +11,7 @@ class WeatherViewController: UIViewController, UITextFieldDelegate, WeatherManag
 
     @IBOutlet weak var searchTextField: UITextField!
     @IBOutlet weak var conditionImageView: UIImageView!
-    @IBOutlet weak var temperatureLAbel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
     
     var weatherManager = WeatherManager()
@@ -51,8 +51,15 @@ class WeatherViewController: UIViewController, UITextFieldDelegate, WeatherManag
         searchTextField.text = ""
     }
     
-    func didUpdateWeather(weather: WeatherModel) {
-        print(weather.temperature)
+    func didUpdateWeather(_ weatherManager: WeatherManager, weather: WeatherModel) {
+        DispatchQueue.main.async {
+            self.temperatureLabel.text = weather.temperatureString
+            self.conditionImageView.image = UIImage(systemName: weather.conditionName)
+        }
+    }
+    
+    func didFailWithError(error: Error) {
+        print(error)
     }
 }
 
